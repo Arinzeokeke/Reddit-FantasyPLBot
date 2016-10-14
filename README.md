9cc952137933af4db5ebcf7bbe65d2d1010b1a64
Okeke Arinze <trent9002@gmail.com>
2016-10-14 14:01:44 +0100

    Remove reduntant code on fpl_bot.py. Add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reddit-FantasyPLBot
+A bot that posts daily Fantasy Premier League possible price rises/drops on the FantasyPL subreddit.
+Running on Heroku.
+Credit.
+- Data Source: www.fplstatistics.co.uk
--- config_skel.py
+++
@@ -1,7 +0,0 @@
-# This file contains the username and password to your bot.
-# DO NOT check it in!
-
-# Fill in the values and rename this file to config_bot.py
-
-REDDIT_USERNAME = "moot"  # YOUR USERNAME as string
-REDDIT_PASS = 'moot'  # YOUR PASSWORD as string
